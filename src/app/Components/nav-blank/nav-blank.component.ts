256a36f995b03de32cd981f2b3ca2221b48b43f7
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-09-21 20:35:01 +0300

    Navigation & footer

--- src/app/Components/nav-blank/nav-blank.component.ts
+++ src/app/Components/nav-blank/nav-blank.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { Router } from 'express';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-blank',
   standalone: true,
-  imports: [],
+  imports: [RouterLink ,RouterLinkActive],
   templateUrl: './nav-blank.component.html',
   styleUrl: './nav-blank.component.scss'
 })
