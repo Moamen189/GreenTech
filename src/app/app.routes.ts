441e72f9ac9a94e33796a4440f943bfcc99ade44
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2024-09-21 19:54:15 +0300

    GreenTechRouting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './Layouts/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './Layouts/blank-layout/blank-layout.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProductComponent } from './Components/product/product.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { CategoriesComponent } from './Components/categories/categories.component';
+import { BrandsComponent } from './Components/brands/brands.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'', component:AuthLayoutComponent , children:[
+    {path:'' , redirectTo:'login' , pathMatch:'full'},
+    {path:'login' , component:LoginComponent},
+    {path:'register' , component:RegisterComponent},
+  ]},
+  {path:'' , component:BlankLayoutComponent , children:[
+    {path:'' , redirectTo:'home' , pathMatch:'full'},
+    {path:'home' , component:HomeComponent},
+    {path:'products' , component:ProductComponent},
+    {path:'cart' , component:CartComponent},
+    {path:'categories' , component:CategoriesComponent},
+    {path:'brands' , component:BrandsComponent},
+  ]},
+  {path:'**' , component:NotfoundComponent}
+];
