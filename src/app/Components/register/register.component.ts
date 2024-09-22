0c670918f62809fc9a3848dc5a6e7c5f2f1dbfa9
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-22 13:40:03 +0300

    Register Form

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  registerForm:FormGroup = new FormGroup({
+    name : new FormControl(null ,[Validators.required , Validators.minLength(3) , Validators.maxLength(20)]),
+    email : new FormControl(null , [Validators.required , Validators.email]),
+    password : new FormControl(null , [Validators.required , Validators.minLength(6)]),
+    rePassword : new FormControl(null , [Validators.required , Validators.minLength(6)]),
+    phone : new FormControl(null , [Validators.required , Validators.minLength(11) , Validators.maxLength(11) , Validators.pattern(/^01[0125][0-9]{8}$/)]),
+  })
+
+  registerSubmit():void{
+      console.log(this.registerForm);
+  }
 
 }
