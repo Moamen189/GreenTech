c684e606452a44c4044792fdeb6a052bda98b075
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-22 14:21:36 +0300

    Login Form

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+  });
+
+  loginSubmit(): void {
+    if (this.loginForm.valid) {
+
+      console.log(this.loginForm);
+
+    }
+
+  }
 }
