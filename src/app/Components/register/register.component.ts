ea42ebde5cc2f6b099c28aec3cd3835b967b0162
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-22 14:00:04 +0300

    Password Confirmation

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,12 +14,21 @@ export class RegisterComponent {
     name : new FormControl(null ,[Validators.required , Validators.minLength(3) , Validators.maxLength(20)]),
     email : new FormControl(null , [Validators.required , Validators.email]),
     password : new FormControl(null , [Validators.required , Validators.minLength(6)]),
-    rePassword : new FormControl(null , [Validators.required , Validators.minLength(6)]),
+    rePassword : new FormControl(null),
     phone : new FormControl(null , [Validators.required , Validators.minLength(11) , Validators.maxLength(11) , Validators.pattern(/^01[0125][0-9]{8}$/)]),
-  })
+  } ,this.confirmPassword);
 
   registerSubmit():void{
       console.log(this.registerForm);
+  }
+
+
+  confirmPassword(G:AbstractControl){
+    if(G.get('password')?.value !== G.get('rePassword')?.value){
+      return {mismatch:true}
+    }else{
+      return null;
+    }
   }
 
 }
