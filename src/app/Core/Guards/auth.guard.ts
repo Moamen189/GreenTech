e43bb1241216071be13741ead44685d44c6e2e8c
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-23 15:28:39 +0300

    Apply Guard

---
+++ src/app/Core/Guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+
+  const _Router = inject(Router);
+
+  if (localStorage.getItem('userToken') === null) {
+    _Router.navigate(['/login']);
+    return false;
+  }else{
+
+    _Router.navigate(['/home']);
+
+    return true;
+  }
+};
