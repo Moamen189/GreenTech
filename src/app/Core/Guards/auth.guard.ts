9a660adf2ebd61baa01999e4c96d5ffec0e759ed
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-23 16:58:38 +0300

    PlatForm Checking

--- src/app/Core/Guards/auth.guard.ts
+++ src/app/Core/Guards/auth.guard.ts
@@ -1,12 +1,14 @@
-import { inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
 
   const _Router = inject(Router);
+  const _PlatForm = inject(PLATFORM_ID);
 
-  if(typeof localStorage !== 'undefined'){
+  if(isPlatformBrowser(_PlatForm)){
      if (localStorage.getItem('userToken') === null) {
     _Router.navigate(['/login']);
     return false;
