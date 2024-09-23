98e52c31e285e3de1065e2244f664c628e39dad1
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-23 14:05:35 +0300

    Base Environement

--- src/app/Core/Services/auth.service.ts
+++ src/app/Core/Services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,10 @@ export class AuthService {
   constructor() { }
 
   setRegisterForm(data:object):Observable<any>{
-    return this._httpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signup',data);
+    return this._httpClient.post(`${environment.baseUrl}/api/v1/auth/signup`,data);
   }
   setLoginForm(data:object):Observable<any>{
-    return this._httpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signin',data);
+    return this._httpClient.post(`${environment.baseUrl}/api/v1/auth/signin`,data);
   }
 
 
