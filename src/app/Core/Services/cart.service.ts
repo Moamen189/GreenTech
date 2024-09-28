fc3c63f81ad73f1bb35c92566693f432e2331d4f
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-28 18:08:35 +0300

    Init Cart Service

---
+++ src/app/Core/Services/cart.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor(private _httpClient:HttpClient) { }
+
+  myHeaders:any = {token: localStorage.getItem('userToken')} ;
+
+  addProductToCart(id:string):Observable<any>{
+    return this._httpClient.post(`${environment.baseUrl}/api/v1/cart`,{"productId":id }, {headers:this.myHeaders});
+  }
+}
