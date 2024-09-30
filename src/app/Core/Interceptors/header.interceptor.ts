d05deb6080125519ba22242e538e876e536f57b1
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-09-30 12:28:40 +0300

    Update

--- src/app/Core/Interceptors/header.interceptor.ts
+++ src/app/Core/Interceptors/header.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const headerInterceptor: HttpInterceptorFn = (req, next) => {
 
-  if(!localStorage.getItem('userToken') !== null)
+  if(localStorage.getItem('userToken') !== null)
     {
       if(req.url.includes('cart') || req.url.includes('wishlist') || req.url.includes('orders')){
         req = req.clone({
