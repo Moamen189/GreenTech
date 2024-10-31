078a4f1caea3ef7d9e0618f5e00f35394c998e02
Moamen Ashraf <Moamen.Ashraf@linkdev.com>
2024-10-31 11:07:43 +0300

    Translate Confiigration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,17 +3,29 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { headerInterceptor } from './Core/Interceptors/header.interceptor';
 import { errorsInterceptor } from './Core/Interceptors/errors.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { loadingInterceptor } from './Core/Interceptors/loading.interceptor';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes , withViewTransitions()),
     provideClientHydration() ,
     provideHttpClient(withFetch() , withInterceptors([headerInterceptor , errorsInterceptor , loadingInterceptor])),
-    provideAnimations() , provideToastr() , importProvidersFrom(NgxSpinnerModule)],
+    provideAnimations() , provideToastr() , importProvidersFrom(NgxSpinnerModule , TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }))],
 };
